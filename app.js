291377863b82e9e324583f83292e6c8b434dc8e0
Vladimir Dubowitskiy <vladimir.dubowitskiy@gmail.com>
2023-08-07 21:03:45 +0300

    refactor: add cors

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const express = require('express');
 const mongoose = require('mongoose'); // Менеджер БД
 const { errors } = require('celebrate'); // Обработчик ошибок валидации
 const helmet = require('helmet');
+const cors = require('cors');
 
 const { errorLogger, requestLogger } = require('./middlewares/logger');
 const limiter = require('./utils/limiter');
 const router = require('./routes/index');
 const errorHandler = require('./middlewares/errorHandler');
-const cors = require('./middlewares/cors');
 
 const { PORT, MONGO_DB } = require('./utils/config');
 
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(helmet()); // Защищаем заголовки
 
 app.use(requestLogger); // Логируем запросы
-app.use(cors);
+app.use(cors());
 
 app.use(limiter); // Ограничиваем кол-во запросов
 app.use(router); // Юзаем роуты
