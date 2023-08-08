ae66127155357ebb47a2b225f5ec920871029c92
Vladimir Dubowitskiy <vladimir.dubowitskiy@gmail.com>
2023-08-08 11:38:33 +0300

    rewrite routes

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,18 @@
 const router = require('express').Router();
-const { celebrate } = require('celebrate');
+// const { celebrate } = require('celebrate');
 
 const auth = require('../middlewares/auth');
 const userRouter = require('./users');
 const movieRouter = require('./movies');
+const authRouter = require('./auth');
 
-const { login, createUser } = require('../controllers/users');
+// const { login, createUser } = require('../controllers/users');
 const NotFound = require('../errors/not-found-error');
-const { createUserValidation, loginValidation } = require('../middlewares/validation');
+// const { createUserValidation, loginValidation } = require('../middlewares/validation');
 
-router.post('/signup', celebrate(createUserValidation), createUser);
-router.post('/signin', celebrate(loginValidation), login);
-
+// router.post('/signup', celebrate(createUserValidation), createUser);
+// router.post('/signin', celebrate(loginValidation), login);
+router.use(authRouter);
 router.use('/users', auth, userRouter); // Защищаем роутеры авторизацией
 router.use('/movies', auth, movieRouter);
 
